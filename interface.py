acf18c93ee0dd0a1e3a1113bee6cab5efc5b5af9
VladUdri <vladudri@gmail.com>
2023-02-21 12:56:23 +0000

    interface almost done

--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import customtkinter
+from save_json import SaveJson
 
 
 class AppInt():
@@ -8,17 +9,35 @@ class AppInt():
         self.root = customtkinter.CTk()
         self.frame = customtkinter.CTkFrame(self.root, corner_radius=10)
         self.checkboxes = []
-        self.checkbox_vars = []
         self.textboxes = []
-        self.textbox_vars = []
+        self.text_voice_command = ''
+        self.apps_for_command = ''
         self.new_button = customtkinter.CTkButton(
             self.frame, text="Add command", command=self.button_fnc)
+        self.save_button = customtkinter.CTkButton(
+            self.frame, text="Save", command=self.save_fnc)
+        self.text_voice_command = customtkinter.CTkEntry(self.frame, width=650, height=40,
+                                                         border_width=1, placeholder_text="Add voice command",
+                                                         text_color="silver")
+        self.apps_for_command = customtkinter.CTkEntry(self.frame, width=650, height=40,
+                                                       border_width=1,
+                                                       placeholder_text="Apps for which should work (optional)",
+                                                       text_color="silver")
+        self.error_label = customtkinter.CTkLabel(master=self.frame,
+                                    textvariable='text_var',
+                                    width=120,
+                                    height=25,
+                                    # fg_color=("white"),
+                                    corner_radius=8)
+
+        self.data = {'voice_command': '',
+                     'apps_command': '', 'actions': [], 'keys': []}
 
     def get_val(self):
         if self.gl == 0:
             return 0
         else:
-            return self.gl-1
+            return self.gl + 1
 
     def refresh(self):
         self.run()
@@ -26,26 +45,40 @@ class AppInt():
     def add_checkbox(self):
         combobox = customtkinter.CTkComboBox(self.frame,
                                              values=[
-                                                 "Press", "Keep pressed"], height=40, state="readonly")
+                                                 "Press", "Keep pressed"], height=40, state="readonly", border_color='red')
         self.checkboxes.append(combobox)
         combobox_var = customtkinter.StringVar(value="Press")
-        self.checkbox_vars.append(combobox_var)
+        self.data['actions'].append(combobox_var)
 
     def add_textbox(self):
         textbox = customtkinter.CTkEntry(self.frame, width=200, height=40,
                                          border_width=1, placeholder_text="Enter a key", text_color="silver")
         self.textboxes.append(textbox)
-        self.textbox_vars.append('')
+        self.data['keys'].append('')
+
+    def save_fnc(self):
+        for i in range(0, self.gl):
+            # print(self.checkboxes[i].get())
+            if self.data['actions'][i] != None and self.data['keys'][i] != '':
+                self.data['actions'][i] = self.checkboxes[i].get()
+                self.data['keys'][i] = self.textboxes[i].get()
+            self.data['voice_command'] = self.text_voice_command.get()
+            self.data['apps_command'] = self.apps_for_command.get()
+
+        SaveJson(self.data).execute()
+        # print(self.data)
 
     def button_fnc(self):
+        if self.gl > 0:
+            self.data['voice_command'] = self.text_voice_command.get()
+            self.data['apps_command'] = self.apps_for_command.get()
         self.gl += 1
         self.add_checkbox()
         self.add_textbox()
         for i in range(0, self.gl):
-            # print(self.checkboxes[i].get())
-            self.checkbox_vars[i] = self.checkboxes[i].get()
-            self.textbox_vars[i] = self.textboxes[i].get()
-        print(self.textbox_vars)
+            self.data['actions'][i] = self.checkboxes[i].get()
+            self.data['keys'][i] = self.textboxes[i].get()
+
         self.refresh()
 
     def run(self):
@@ -57,17 +90,37 @@ class AppInt():
         self.root.title('Add commands')
         self.root.geometry("800x500")
 
-        self.frame.grid(row=0, column=0, padx=10,
-                        pady=10, sticky=customtkinter.W)
+        self.frame.grid(pady=10, sticky="we")
+        self.frame.grid_columnconfigure(0, weight=1)
+        self.frame.grid_rowconfigure(1, weight=1)
+        self.frame.pack()
+
+        self.root.grid_columnconfigure(0, weight=1)
+
+        if self.gl > 0:
+            self.text_voice_command.grid(
+                row=0, column=0, padx=10, pady=10, columnspan=3)
+
+            self.apps_for_command.grid(
+                row=1, column=0, padx=10, pady=10, columnspan=3)
 
         for i in range(0, self.gl):
-
-            self.checkboxes[i].grid(row=i, column=0, padx=10,
-                                    pady=10, sticky=customtkinter.W)
+            self.checkboxes[i].grid(row=i + 2, column=0, padx=10,
+                                    pady=10)
             self.textboxes[i].grid(
-                row=i, column=1, padx=(0, 10), pady=10, sticky="ew")
+                row=i + 2, column=1, padx=10, pady=10)
 
-        self.new_button.grid(row=self.get_val(), column=2, padx=50,
+        self.new_button.grid(row=self.get_val(), column=2, padx=10,
+                             pady=10)
+        if self.gl > 0:
+            self.save_button.grid(row=self.gl + 2, column=1, padx=100,
+                                  pady=10)
+
+        # if self.error ==
+#         text_var = tkinter.StringVar(value="CTkLabel")
+
+        
+        self.error_label.grid(row=self.gl + 3, column=1, padx=10,
                              pady=10)
 
         self.root.mainloop()
---
+++ save_json.py
@@ -0,0 +1,42 @@
+import pyautogui as pg
+from time import sleep
+import json
+
+
+class SaveJson:
+    def __init__(self, data) -> None:
+        self.data = data
+        with open('custom_commands.json') as f:
+            self.json_data = json.load(f)
+
+    def __translate(self, action):
+        match action:
+            case 'Press':
+                return 'press'
+            case 'Keep pressed':
+                return 'key_down'
+
+    def create_action_list(self):
+        action_list = []
+        for i in range(0, len(self.data['actions'])):
+            action_list.append(self.__translate(self.data['actions'][i]))
+            action_list.append(self.data['keys'][i])
+            if i > 0 and self.data['actions'][i - 1] == 'Keep pressed' and self.data['actions'][i] == 'Press':
+                action_list.append('key_up')
+                action_list.append(self.data['keys'][i - 1])
+        return action_list
+        # todo do here
+
+    def __write_json(self):
+        with open('custom_commands.json', 'w') as f:
+            json.dump(self.json_data, f)
+
+    def execute(self):
+        action_list = self.create_action_list()
+        print(self.json_data)
+        self.json_data[self.data['voice_command']] = {}
+        self.json_data[self.data['voice_command']]['execute'] = action_list
+        self.json_data[self.data['voice_command']
+                       ]['apps'] = self.data['apps_command']
+        self.__write_json()
+        print(self.json_data)
